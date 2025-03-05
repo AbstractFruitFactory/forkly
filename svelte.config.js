2d43eaf53c7cae4144051e83dac00a15a4ff4d56
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-03-05 11:50:08 +0000

    chore: configure Vercel adapter for SvelteKit deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-vercel'
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +14,6 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
-};
+}
 
-export default config;
+export default config
