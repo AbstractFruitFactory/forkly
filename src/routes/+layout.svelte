3ec130641bdc5f17a0f2ebb3627970080ef9258b
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-03-31 22:46:05 +0100

    chore: consolidate font links and update HTML structure in app and Storybook preview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,23 +8,9 @@
 	let { children, data }: { children: Snippet; data: LayoutData } = $props()
 </script>
 
-<svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Playfair+Display:wght@500;600;700&family=Pacifico&display=swap"
-		rel="stylesheet"
-	/>
-</svelte:head>
-
 <Layout>
 	{#snippet header()}
-		<Header
-			loggedIn={!!data.user}
-			newRecipeHref="/new"
-			profileHref="/profile"
-			loginHref="/login"
-		/>
+		<Header loggedIn={!!data.user} newRecipeHref="/new" profileHref="/profile" loginHref="/login" />
 	{/snippet}
 
 	{#snippet content()}
