689d33ccce483ae8e23bdc2e5288302e78214654
AbstractFruitFactory <alexmedkex@gmail.com>
2025-06-18 16:36:03 +0100

    feat(recipe): animate red like

--- src/lib/components/floating-action-button/FloatingLikeButton.svelte
+++ src/lib/components/floating-action-button/FloatingLikeButton.svelte
@@ -14,7 +14,21 @@
 <style lang="scss">
 	.like-button-container {
 		:global(.action-button.active > svg) {
-			fill: var(--color-text);
+			color: var(--color-error);
+			fill: var(--color-error);
+			animation: like-pop 0.3s ease;
+		}
+	}
+
+	@keyframes like-pop {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(1.3);
+		}
+		100% {
+			transform: scale(1);
 		}
 	}
 </style>
