e4996d1c59f30b03c158885989bd76b629236495
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-07-02 08:05:03 +0100

    fix: remove back button on recipe page

--- src/lib/pages/recipe/MobileLayout.svelte
+++ src/lib/pages/recipe/MobileLayout.svelte
@@ -29,12 +29,6 @@
 </script>
 
 <div class="recipe" data-page="recipe">
-	<button class="back-button" onclick={onBackClick}>
-		<svg width="24" height="24" viewBox="0 0 24 24">
-			<path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
-		</svg>
-	</button>
-
 	<div class="recipe-image">
 		{@render image()}
 	</div>
