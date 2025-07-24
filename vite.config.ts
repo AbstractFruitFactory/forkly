b0c426999715e65cc7183eb3b2659787e0f7fd8b
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-07-24 10:48:18 +0100

    chore: allow grok host

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: [
+			'localhost',
+			'127.0.0.1',
+			'.ngrok-free.app',
+			'.ngrok.io'
+		]
+	}
 });
