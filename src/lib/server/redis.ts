7fe63f6ca8dc099e3846b2b39d3bed5a27ab8a2d
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-07-27 19:37:44 +0100

    fix: redis

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,14 +1,10 @@
-import { createClient } from '@redis/client'
-// import { REDIS_URL } from '$env/static/private'
+import { Redis } from '@upstash/redis'
 import type { IngredientSearchResult } from './food-api'
 
-const redis = createClient({
-  url: process.env.REDIS_URL
+const redis = new Redis({
+  url: process.env.KV_REST_API_URL,
+  token: process.env.KV_REST_API_TOKEN
 })
-
-redis.on('error', (err: Error) => console.error('Redis Client Error', err))
-
-await redis.connect()
 
 const INGREDIENT_CACHE_PREFIX = 'ingredient:'
 const INGREDIENT_CACHE_TTL = 60 * 60 * 24 // 24 hours
@@ -17,7 +13,7 @@ export const ingredientCache = {
   set: async (name: string, ingredient: IngredientSearchResult[0]) => {
     const key = INGREDIENT_CACHE_PREFIX + name.toLowerCase()
     await redis.set(key, JSON.stringify(ingredient), {
-      EX: INGREDIENT_CACHE_TTL
+      ex: INGREDIENT_CACHE_TTL
     })
   },
 
@@ -27,7 +23,7 @@ export const ingredientCache = {
     if (!cached) {
       return null
     }
-    return JSON.parse(cached)
+    return JSON.parse(cached as string)
   }
 }
 
