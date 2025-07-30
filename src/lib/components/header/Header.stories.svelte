030f6c149055978d67795fe26564b702184dd708
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-07-30 16:48:19 +0100

    fix: header story

--- src/lib/components/header/Header.stories.svelte
+++ src/lib/components/header/Header.stories.svelte
@@ -1,33 +1,56 @@
 <script lang="ts" module>
 	import { defineMeta } from '@storybook/addon-svelte-csf'
 	import Header from './Header.svelte'
+	import type { Component, ComponentProps } from 'svelte'
 
-	const { Story } = defineMeta({
+	const { Story } = defineMeta<Component<ComponentProps<typeof Header>>>({
 		title: 'lib/components/Header',
 		component: Header,
 		tags: ['autodocs'],
 		parameters: {
 			layout: 'fullscreen'
+		},
+		argTypes: {
+			loggedIn: {
+				control: 'boolean',
+				defaultValue: false,
+				name: 'logged in'
+			},
+			profilePicUrl: {
+				control: 'text',
+				defaultValue: 'https://i.pravatar.cc/150?u=user123',
+				name: 'profile picture URL'
+			},
+			actions: {
+				table: { disable: true }
+			},
+			logo: {
+				table: { disable: true }
+			},
+			newRecipeHref: {
+				table: { disable: true }
+			},
+			profileHref: {
+				table: { disable: true }
+			},
+			loginHref: {
+				table: { disable: true }
+			}
 		}
 	})
 </script>
 
 <Story name="Logged Out">
 	{#snippet children(args)}
-		<div style="padding: 0 20px; background-color: #1a1a1a;">
-			<Header
-				loggedIn={false}
-				newRecipeHref="/new-recipe"
-				loginHref="/login"
-				{...args}
-			/>
+		<div style="padding: 0 20px;">
+			<Header loggedIn={false} newRecipeHref="/new-recipe" loginHref="/login" {...args} />
 		</div>
 	{/snippet}
 </Story>
 
 <Story name="Logged In">
 	{#snippet children(args)}
-		<div style="padding: 0 20px; background-color: #1a1a1a;">
+		<div style="padding: 0 20px;">
 			<Header
 				loggedIn={true}
 				newRecipeHref="/new-recipe"
@@ -41,27 +64,8 @@
 
 <Story name="Logged In No Profile Pic">
 	{#snippet children(args)}
-		<div style="padding: 0 20px; background-color: #1a1a1a;">
-			<Header
-				loggedIn={true}
-				newRecipeHref="/new-recipe"
-				profileHref="/profile"
-				{...args}
-			/>
+		<div style="padding: 0 20px;">
+			<Header loggedIn={true} newRecipeHref="/new-recipe" profileHref="/profile" {...args} />
 		</div>
 	{/snippet}
 </Story>
-
-<Story name="Mobile View">
-	{#snippet children(args)}
-		<div style="padding: 0 20px; background-color: #1a1a1a; max-width: 480px;">
-			<Header
-				loggedIn={true}
-				newRecipeHref="/new-recipe"
-				profileHref="/profile"
-				profilePicUrl="https://i.pravatar.cc/150?u=user123"
-				{...args}
-			/>
-		</div>
-	{/snippet}
-</Story> 
