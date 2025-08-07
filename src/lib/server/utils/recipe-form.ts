1699a4546eb7f75835f067bcadde58c3c8cee2d7
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2025-08-07 18:35:56 +0100

    fix: rework ingredient formatting

--- src/lib/server/utils/recipe-form.ts
+++ src/lib/server/utils/recipe-form.ts
@@ -282,6 +282,8 @@ export const buildRecipePayloadFromForm = async (formData: FormData, skipValidat
     }
   }
 
+  console.log('ingredients', allIngredients)
+
   let nutrition: typeof recipeData.manualNutrition | null = null
   if (recipeData.nutritionMode === 'manual') {
     nutrition = recipeData.manualNutrition ?? null
